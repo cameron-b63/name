7af3d1e9d9d74c6b642541ea60c50aeb05be7eda
Zach Allwein <zrallwein@gmail.com>
2025-01-08 20:27:20 -0600

    removed refrences from SrcSpan

--- name-as/src/assembler/assembler.rs
+++ name-as/src/assembler/assembler.rs
@@ -36,7 +36,7 @@ pub enum ErrorKind {
     UnknownInstruction(String),
 }
 
-pub type AssembleError<'a> = Span<'a, ErrorKind>;
+pub type AssembleError = Span<ErrorKind>;
 
 // This file contains the struct definition and extracted functions used in the assembler_logic file. There was far too much inlined, so I have extracted it.
 
@@ -277,7 +277,7 @@ impl Assembler {
                     src_span: ast.src_span,
                     kind: err,
                 });
-            })
+            });
         }
 
         // process line info
@@ -309,7 +309,17 @@ impl Assembler {
             self.line_number += 1;
         }
 
-        dbg!(&errors);
+        for error in errors.iter() {
+            println!("error: {:?}", error.kind);
+            println!(
+                "\t--> {}:{}:{}",
+                path.display(),
+                error.src_span.start.line,
+                error.src_span.start.line_pos
+            );
+            println!("\t| {}", &content[error.src_span.range()])
+        }
+
         errors.is_empty()
     }
 
