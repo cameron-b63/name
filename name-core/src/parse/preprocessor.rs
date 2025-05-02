5438ee47da527147cac15d579b6045f681061441
Zenna Allwein <zrallwein@gmail.com>
2025-05-01 19:21:23 -0500

    implement error reporting infrastructure

--- name-core/src/parse/preprocessor.rs
+++ name-core/src/parse/preprocessor.rs
@@ -1,15 +1,15 @@
 use crate::parse::lexer::Lexer;
 use crate::parse::session::Session;
 use crate::parse::token::{Token, TokenCursor, TokenKind};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
-pub struct Preprocessor<'a> {
-    sess: &'a mut Session<'a>,
-    eqvs: HashMap<&'a str, Vec<Token<'a>>>,
+pub struct Preprocessor<'sess, 'sess_ref> {
+    sess: &'sess_ref mut Session<'sess>,
+    eqvs: HashMap<&'sess str, Vec<Token>>,
 }
 
-impl<'a> Preprocessor<'a> {
-    pub fn new(sess: &'a mut Session<'a>) -> Self {
+impl<'sess, 'sess_ref> Preprocessor<'sess, 'sess_ref> {
+    pub fn new(sess: &'sess_ref mut Session<'sess>) -> Self {
         Preprocessor {
             sess,
             eqvs: HashMap::new(),
@@ -18,35 +18,39 @@ impl<'a> Preprocessor<'a> {
 
     /// The preprocessor is responsible for expanding our expandables.
     /// Anything that performs something akin to string replacement happens here.
-    pub fn preprocess(&mut self, toks: Vec<Token<'a>>) -> Vec<Token<'a>> {
-        let mut cursor = TokenCursor::new(toks);
-        let mut tokens = Vec::new();
+    pub fn preprocess(&mut self, mut cursor: TokenCursor) -> TokenCursor {
+        // let mut cursor = TokenCursor::new(toks);
+        let mut tokens = VecDeque::new();
 
         while let Some(tok) = cursor.next() {
-            match tok.token.kind {
-                TokenKind::Directive => match tok.src {
+            match tok.kind {
+                TokenKind::Directive => match self.sess.get_src_str(&tok.src_span) {
                     ".include" => {
-                        let src = cursor.next_if(TokenKind::String).unwrap().src;
+                        let src_span = &cursor.next_if(TokenKind::String).unwrap().src_span;
+                        let src = self.sess.get_src_str(src_span);
                         let file_name = &src[1..src.len() - 1];
-                        let cont = self.sess.add_file(&self.sess.dir.join(file_name));
 
-                        let mut lexer = Lexer::new(cont);
+                        let file = self.sess.add_file(self.sess.dir.join(file_name));
+
+                        let mut lexer = Lexer::new(&file.str, file.src_span.pos);
                         //Todo: handle errors
-                        let (_errs, toks) = lexer.lex();
+                        let (_errs, cursor) = lexer.lex();
 
-                        let pre = self.preprocess(toks);
-                        tokens.extend(pre);
+                        let pre = self.preprocess(cursor);
+                        tokens.extend(pre.toks);
                     }
                     ".eqv" => {
-                        let ident = cursor.next_if(TokenKind::Ident).unwrap().src;
+                        let src_span = &cursor.next_if(TokenKind::Ident).unwrap().src_span;
+                        let ident = self.sess.get_src_str(src_span);
+
                         let mut expansion = Vec::new();
 
                         // read until and consume the newline but ignore it
-                        while let Some(tok) = cursor
-                            .next()
-                            .filter(|tok| !tok.token.is_kind(TokenKind::Newline))
+                        while let Some(tok) =
+                            cursor.next().filter(|tok| !tok.is_kind(TokenKind::Newline))
                         {
-                            if let Some(c_expansion) = self.eqvs.get(tok.src) {
+                            let src = self.sess.get_src_str(&tok.src_span);
+                            if let Some(c_expansion) = self.eqvs.get(src) {
                                 expansion.extend(c_expansion.clone());
                             } else {
                                 expansion.push(tok.clone())
@@ -56,18 +60,19 @@ impl<'a> Preprocessor<'a> {
                         let _ = self.eqvs.insert(ident, expansion);
                     }
                     ".macro" => todo!("implement macros"),
-                    _ => tokens.push(tok.clone()),
+                    _ => tokens.push_back(tok.clone()),
                 },
                 TokenKind::Ident => {
-                    if let Some(expansion) = self.eqvs.get(tok.src) {
+                    let src = self.sess.get_src_str(&tok.src_span);
+                    if let Some(expansion) = self.eqvs.get(src) {
                         tokens.extend(expansion.clone());
                     } else {
-                        tokens.push(tok.clone())
+                        tokens.push_back(tok.clone())
                     }
                 }
-                _ => tokens.push(tok.clone()),
+                _ => tokens.push_back(tok.clone()),
             }
         }
-        tokens
+        TokenCursor::new(tokens)
     }
 }
--- name-core/src/parse/session.rs
+++ name-core/src/parse/session.rs
@@ -1,13 +1,100 @@
-use crate::parse::span::Span;
+use crate::parse::span::SrcSpan;
 use bumpalo::Bump;
-use std::fmt;
 use std::fs;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
+pub struct File {
+    path: PathBuf,
+    pub str: String,
+    pub src_span: SrcSpan,
+    lines: Vec<SrcSpan>,
+}
+
+impl File {
+    pub fn new(path: PathBuf, str: String, pos: usize) -> Self {
+        let mut last_pos = pos;
+
+        let lines: Vec<SrcSpan> = str
+            .lines()
+            .map(|line| {
+                let pos = last_pos;
+                let length = line.len() + 1;
+                last_pos += length;
+                SrcSpan { pos, length }
+            })
+            .collect();
+
+        File {
+            path,
+            src_span: SrcSpan {
+                pos,
+                length: str.len(),
+            },
+            str,
+            lines,
+        }
+    }
+
+    pub fn get_line_column_str(&self, src_span: &SrcSpan) -> (usize, usize, &str) {
+        let line_idx = self
+            .lines
+            .binary_search_by(|cmp_src_span| cmp_src_span.pos.cmp(&src_span.pos))
+            .unwrap_or_else(|line_idx| line_idx - 1);
+
+        let column = src_span.pos - self.lines[line_idx].pos;
+
+        let start = src_span.pos - self.src_span.pos;
+
+        let str = &self.str.get(start..(start + src_span.length)).unwrap_or("");
+
+        (line_idx + 1, column, str)
+    }
+}
+
+#[derive(Debug)]
+pub struct Src<'a> {
+    files: Vec<&'a File>,
+    pub length: usize,
+}
+
+impl<'a> Src<'a> {
+    pub fn new() -> Self {
+        Src {
+            files: Vec::new(),
+            length: 0,
+        }
+    }
+
+    pub fn add_file(&mut self, file: &'a File) {
+        self.length = self.length + file.src_span.length;
+        self.files.push(file);
+    }
+
+    pub fn get_span_details(&self, src_span: &SrcSpan) -> (&'a Path, &'a str, usize, usize) {
+        // dbg!(&self);
+        let file_idx = self
+            .files
+            .binary_search_by(|file| file.src_span.pos.cmp(&src_span.pos))
+            .unwrap_or_else(|file_idx| file_idx - 1);
+
+        let file = self.files[file_idx];
+
+        let (line, column, str) = file.get_line_column_str(src_span);
+
+        (&file.path, str, line, column)
+    }
+
+    pub fn get_str(&self, src_span: &SrcSpan) -> &'a str {
+        let (_, cont, _, _) = self.get_span_details(src_span);
+        cont
+    }
+}
+
+#[derive(Debug)]
 pub struct Session<'a> {
     bump: &'a Bump,
-    buffers: Vec<&'a str>,
+    pub src: Src<'a>,
     pub dir: PathBuf,
     pub should_assemble: bool,
 }
@@ -16,25 +103,33 @@ impl<'a> Session<'a> {
     pub fn new(bump: &'a Bump, dir: PathBuf) -> Session<'a> {
         Session {
             bump,
-            buffers: Vec::new(),
+            src: Src::new(),
             dir,
             should_assemble: true,
         }
     }
 
-    pub fn add_file(&mut self, path: &Path) -> &'a str {
+    pub fn add_file(&mut self, path: PathBuf) -> &'a File {
         let cont = fs::read_to_string(&path).unwrap();
-        self.add_src(cont)
+        let file = self.bump.alloc(File::new(path, cont, self.src.length));
+        self.src.add_file(file);
+        file
     }
 
-    pub fn add_src(&mut self, cont: String) -> &'a str {
-        let src = self.bump.alloc(cont);
-        self.buffers.push(src);
-        src
+    pub fn get_src_str(&self, src_span: &SrcSpan) -> &'a str {
+        self.src.get_str(src_span)
     }
 
-    pub fn report_error<T: fmt::Display>(&mut self, error: Span<T>) {
+    pub fn report_error(&mut self, err: &str, src_span: &SrcSpan) {
         self.should_assemble = false;
-        println!("{}", error);
+
+        let (file, str, mut line, col) = self.src.get_span_details(&src_span);
+
+        println!("error: {}\n\t--> {:?}:{}:{}", err, file, line, col);
+
+        for str_line in str.lines() {
+            println!("\t{} | {}", line, str_line);
+            line += 1;
+        }
     }
 }
