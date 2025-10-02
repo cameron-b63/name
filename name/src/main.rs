2936e947d6ebd576e0f563adc8be89e05664e9c0
Zenna Allwein <zrallwein@gmail.com>
2025-10-02 12:43:51 -0500

    buildtool now supports directories and implicit builds

--- name/src/main.rs
+++ name/src/main.rs
@@ -1,32 +1,47 @@
 use clap::Parser;
-use name_as::args::Cli;
+use std::env;
 
 #[derive(Parser, Debug)]
 struct Args {
-    pub path: std::path::PathBuf,
+    pub path: Option<std::path::PathBuf>,
 }
 
 fn main() -> Result<(), String> {
     let args = Args::parse();
 
-    let mut output = args.path.clone();
-    let _ = output.set_extension("o");
+    let path = args
+        .path
+        .unwrap_or(env::current_dir().expect("Could not find a path to assemble"));
 
-    let assemble_args = Cli {
-        input_filename: args.path.clone(),
-        output_filename: output,
-        verbose: false,
-    };
+    let mut input_files = vec![];
 
-    let elf = name_as::run_assembler(&assemble_args);
+    if path.is_dir() {
+        for entry in path.read_dir().expect("Could not read dir") {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_file() && path.extension().is_some_and(|ext| ext == "asm") {
+                    input_files.push(path);
+                }
+            }
+        }
+    } else if path.is_file() && path.extension().is_some_and(|ext| ext == "asm") {
+        input_files.push(path);
+    }
 
-    // Invoke linker on collected Elfs
-    let executable_contents = match name_ld::linker::linker(vec![elf]) {
+    if input_files.is_empty() {
+        eprintln!("Could not find files to asemble");
+        std::process::exit(1);
+    }
+
+    let elfs = input_files
+        .into_iter()
+        .map(|file| name_as::run_assembler(file))
+        .collect();
+
+    let executable_contents = match name_ld::linker::linker(elfs) {
         Ok(elf) => elf,
         Err(e) => panic!("{e}"),
     };
-
-    dbg!(&executable_contents);
 
     name_emu::simulator::simulate(executable_contents, false, false)
 }
